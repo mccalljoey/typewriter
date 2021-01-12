376e0c245d60bd2ad2e53e90de5b1044621ec127
mccalljoey <mccalljoey@gmail.com>
2021-01-12 18:03:16 +0000

    it type writes each letter individually

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i])
+  }, i * 50)};
+
+setTimeout(() => {
+  process.stdout.write('\n')
+}, sentence.length * 50 + 50);
